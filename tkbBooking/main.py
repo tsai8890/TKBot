0cbf824631e85d95081e00f20d6a605917d9e256
tsai8890 <tsai8890@users.noreply.github.com>
2023-08-31 07:36:35 +0000

    build: auto-packing the executable

---
+++ tkbBooking/main.py
@@ -0,0 +1,17 @@
+import sys
+sys.dont_write_bytecode = True
+
+from modules.gui.windows import main_loop
+from modules.tkb_agent.tkb_agent import TKB_Agent
+import modules.globals as globals
+
+
+def main():
+    globals.init_globals()
+    tkb_agent = TKB_Agent()
+    main_loop(tkb_agent)
+    # tkb_agent.driver.quit()
+
+
+if __name__ == '__main__':
+    main()
