75e70758838361cae689aa7c6d7eaee4f298c765
tsai8890 <tsai8890@users.noreply.github.com>
2023-08-31 07:57:34 +0000

    build: auto-packing the executable

---
+++ tkbBooking/modules/globals/globals.py
@@ -0,0 +1,11 @@
+import json
+
+
+def init_globals():
+    # tkb_data
+    global tkb_data
+    file = open('modules/gui/tkb_data.json')
+    tkb_data = json.load(file)
+
+    global MAX_SESSIONS
+    MAX_SESSIONS = 6
